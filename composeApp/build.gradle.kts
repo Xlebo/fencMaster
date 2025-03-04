c6b8d5f89a505a7540ff087be5b71fb94d2896e6
Xlebo <samo.chlebo@gmail.com>
2025-03-04 19:47:37 +0100

    Fix icon for distributable

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -118,7 +118,12 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb, TargetFormat.Exe)
             packageName = "fencmaster"
-            packageVersion = "1.0.0"
+            packageVersion = "1.0.2"
+            windows {
+                iconFile.set(project.file("mec.ico"))
+                shortcut = true
+                menu = true
+            }
         }
     }
 }
